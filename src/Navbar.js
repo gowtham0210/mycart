86bcba4ffe59b1b0c52fc08b0058cd0a0c785ab9
gowtham0210 <gowthambujju02@gmail.com>
2022-07-05 21:05:59 +0530

    fourth commit

--- src/Navbar.js
+++ src/Navbar.js
@@ -3,15 +3,18 @@ import bag from './media/Icons/bag.svg';
 
 function Navbar() {
   return (
-    <div>
+    <nav>
         <div className='nav-center'>
             <h3>Hot Deals</h3>
             <div className='nav-container'>
                 <img src={bag} alt="bag" />
+                <div className='amount-contained'>
+                    <p className='total-amount'>5</p>
+                </div>
 
             </div>
         </div>
-    </div>
+    </nav>
   )
 }
 
