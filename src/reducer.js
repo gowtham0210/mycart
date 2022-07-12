2ea49defb6383c50f65de5d937daf17bdde9a901
gowtham0210 <gowthambujju02@gmail.com>
2022-07-12 13:10:47 +0530

    12th commit

--- src/reducer.js
+++ src/reducer.js
@@ -1,5 +1,3 @@
-
-
 const reducer = (state, action) =>{
     if(action.type === "CLEAR_CART")
     {
@@ -35,6 +33,48 @@ const reducer = (state, action) =>{
         }).filter((cartitem) => cartitem !==0)
         return {...state, cart:tempcart}
     }
+
+    if(action.type === "GET_TOTALS")
+    {
+    let {total , amount } = state.cart.reduce(
+               (cartTotal, cartitem) =>{
+                            const {price, amount} = cartitem;
+                            const itemTotal = price * amount;
+
+                            cartTotal.total += itemTotal;
+                            cartTotal.amount += amount;
+               },
+               {
+                total:0,
+                amount:0,
+               }
+    );
+    total  = parseFloat(total.toFixed(2));
+    return {...state, total, amount};
+    }
+
+    if(action.type === "LOADING"){
+        return {...state, loading:true}
+    }
+    if(action.type === "DISPLAY_ITEMS")
+    {
+        return {...state, cart:action.payload,loading:false};
+    }
+    if(action.type === "TOGGLE_AMOUNT"){
+        let tempcart = state.cart.map((cartitem) =>{
+            if(cartitem.id === action.payload){
+                if(action.payload.type === "inc"){
+                 return {...cartitem, amount:cartitem.amount + 1}
+            }
+        
+            if(action.payload.type === "dec"){
+                return {...cartitem, amount:cartitem.amount - 1}
+            }
+        } return cartitem;
+        } ).filter((cartitem) => cartitem.amount !== 0);
+        return {...state, cart: tempcart};
+    }
+    throw new Error("no matching Action type");
 };
 
 export default reducer;
