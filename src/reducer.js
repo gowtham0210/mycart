765002d1e4ec241e8625cf5d25a934fe65dec0f1
gowtham0210 <gowthambujju02@gmail.com>
2022-07-12 11:15:47 +0530

    12th commit

--- src/reducer.js
+++ src/reducer.js
@@ -1,5 +1,40 @@
+
+
 const reducer = (state, action) =>{
-    return state;
+    if(action.type === "CLEAR_CART")
+    {
+        return {...state, cart:[]};
+    }
+    if(action.type === "REMOVE")
+    {
+        return{
+            ...state, cart: state.cart.filter((cartitem) => cartitem.id !== action.payload),
+        };
+    }
+    if(action.type === "INCREASE"){
+        let tempcart = state.cart.map((cartitem) =>{
+            if(cartitem.id === action.payload){
+                return {
+                    ...cartitem,
+                    amount:cartitem+1
+                };
+            }
+            return cartitem;
+        })
+        return {...state, cart:tempcart}
+    }
+    if(action.type === "DECREASE"){
+        let tempcart = state.cart.map((cartitem) =>{
+            if(cartitem.id === action.payload){
+                return {
+                    ...cartitem,
+                    amount:cartitem-1
+                };
+            }
+            return cartitem;
+        }).filter((cartitem) => cartitem !==0)
+        return {...state, cart:tempcart}
+    }
 };
 
 export default reducer;
